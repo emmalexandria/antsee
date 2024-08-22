3dcf79462b41ec5c50d7bfb7ddfa9f0e8e246a93
Emma Alexandria <emma.jellemabutler@gmail.com>
2024-08-22 16:44:42 +0200

    Add display example

--- examples/display.rs
+++ examples/display.rs
@@ -14,7 +14,10 @@ fn main() {
         "library.".with_style(&intro_style)
     );
 
-    println!("It has full support for the 256 color palette");
+    println!(
+        "{}",
+        "It has full support for the 256 color palette:".with_style(&intro_style)
+    );
     let mut col = 0;
     for i in 0..=255 {
         let mut num_str = i.to_string();
@@ -28,17 +31,31 @@ fn main() {
         }
     }
 
-    println!("And it can display RGB colors");
-    col = 0;
-    for i in 0..=255 {
-        print!("{}", " ".to_styled().with_bg(RGB::rgb(i, 0, 0)));
+    println!(
+        "\n{}",
+        "And it can display RGB colors:".with_style(&intro_style)
+    );
 
-        col += 1;
-        if col == 32 {
-            col = 0;
+    let mut curr_col = 0;
+    let mut abs_row = 0;
+    for _ in 0..32u32.pow(2) {
+        print!(
+            "{}",
+            "  ".to_styled().with_bg(RGB::rgb(
+                map_in_rgb_range(abs_row, 0, 32),
+                0,
+                map_in_rgb_range(curr_col, 0, 32)
+            ))
+        );
+        curr_col += 1;
+        if curr_col == 32 {
+            curr_col = 0;
             print!("\n");
+            abs_row += 1;
         }
     }
+
+    println!("");
 }
 
 fn pad_with_zeros(num: &mut String) {
@@ -47,4 +64,9 @@ fn pad_with_zeros(num: &mut String) {
     }
     num.insert(0, ' ');
     num.push(' ');
+}
+
+fn map_in_rgb_range(val: u8, min: u8, max: u8) -> u8 {
+    let range = max - min;
+    return (255.0 * (f32::from(val) / f32::from(range))) as u8;
 }
